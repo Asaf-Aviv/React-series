0fcdb5fb17ac50645e0af04c0afe2a6b69cc165f
AsafAviv <asafaviv89@gmail.com>
2018-07-20 00:29:11 +0300

    add fortnite api

---
+++ api/fortnite/fortniteAPI.js
@@ -0,0 +1,6 @@
+const axios = require('axios');
+
+headers = {
+  Authorization: pricess.env.FORTNITE_KEY,
+  'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
+}
--- client/src/Fortnite/Fortnite.js
+++ client/src/Fortnite/Fortnite.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import axios from 'axios';
+import qs from 'querystring';
 
+axios.defaults.baseURL = 'https://fortnite-public-api.theapinetwork.com/prod09/';
+axios.defaults.headers.common['Authorization'] = ''
 class Fortnite extends Component {
-  static propTypes = {
-    prop: PropTypes
-  };
+  componentDidMount = () => {
+    this.getPVEInfo()
+      .then(res => console.log(res))
+
+  }
+
+  getUser = username => axios.post('users/id', qs.stringify({ username: 'ninja' }))
+
+  getUserStats = ({ uid, platforms }, window = 'alltime') => 
+    axios.post('users/public/br_stats', qs.stringify({ user_id: uid, platform: platforms[0], window }))
+  
+  getStore = () => axios.post('store/get', qs.stringify({ language: 'en' }))
+  
+  getUpcomingItems = () => axios.post('upcoming/get', qs.stringify({ language: 'en' }))
+
+  getStdNews = (language = 'en') => axios.post('stw_motd/get', qs.stringify({ language }))
+  
+  getBrNews = (language = 'en') => axios.post('br_motd/get', qs.stringify({ language }))
+  
+  getBrChallenges = (season = 'season4' ) => axios.post('challenges/get', qs.stringify({ season, language: 'en' }))
+
+  getLeaderboards = (platform, gameMode, window) => axios.post('challenges/get', qs.stringify({ platform, gameMode, window: 'top_1_wins' }))
+  
+  getTop10KillsPlayers = () => axios.post('challenges/get', qs.stringify({ window: 'top_10_kills', language: 'en' }))
+  
+  getPatchNots = () => axios.post('patchnotes/get')
+
+  getPVEInfo = () => axios.post('pveinfo/get')
 
   render() {
     return (
